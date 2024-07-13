663f8eef1a1e19e3a199feb5e460f0f3fc97f67a
dougjhall <twowidedev@gmail.com>
2024-07-13 16:26:39 -0500

    added comments

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from data import *
 from linkedlist import LinkedList
 
+#outline supported types of pizza
 all_pizza_types = ['chicken', 'veggie', 'pepperoni', 'sausage']
 
+#function to load the linked lists
 def create_pizza_date():
     pizza_data_list = LinkedList()
+    #this ensure that all the pizza with the same types are grouped together
     for type in all_pizza_types:
         pizza_sublist = LinkedList()
         for pizza in pizza_data:
@@ -16,6 +19,7 @@ def create_pizza_date():
 all_pizza_data = create_pizza_date()
 valid_type = False
 
+#user prompt
 while valid_type == False:
     user_input = str(input("What type of pizza would you like? (chicken, veggie, pepperoni, sausage) ")).lower()
     if user_input not in all_pizza_types:
@@ -23,6 +27,7 @@ while valid_type == False:
     else:
         valid_type = True
 
+#loop through linked lists and print the pizzas that match the type the user entered
 print("Here are some pizza options you will like!")
 pizza_list_head = all_pizza_data.get_head_node()
 while pizza_list_head.get_next_node() is not None:
